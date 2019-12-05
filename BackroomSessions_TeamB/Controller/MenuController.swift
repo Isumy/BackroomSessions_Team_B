f652d1e8808eaaf5242591e1dc5bc77d5da5f88c
Isumy Aguila <iagui032@fiu.edu>
2019-12-05 00:50:17 -0500

    Created Model for MenuOption. Modified MenuViewController to conform to UITableViewDelegate and UIDatasource. Populated menuTableView.

--- BackroomSessions_TeamB/Controller/MenuController.swift
+++ BackroomSessions_TeamB/Controller/MenuController.swift
@@ -22,6 +22,7 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
      //Init
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
     }
     
    
@@ -32,7 +33,23 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
         menuTableView.delegate  = self
         menuTableView.dataSource = self
         
+        //registering the view cell
         menuTableView.register(MenuOptionViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        menuTableView.backgroundColor = .darkGray
+        menuTableView.separatorStyle  = .none
+        menuTableView.rowHeight  = 80
+        
+        //Adding menutableview to the subview
+        view.addSubview(menuTableView)
+        //Creating constraints
+        menuTableView.translatesAutoresizingMaskIntoConstraints = false
+        menuTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        menuTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        menuTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        menuTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,7 +59,11 @@ class MenuController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let cell = menuTableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MenuOptionViewCell
-        
+        //Populating table view
+        let menuOption = MenuOption(rawValue: indexPath.row)
+        //getting the values 
+        cell.descriptionLabel.text = menuOption?.description
+        cell.iconImageView.image = menuOption?.image
         
         return cell
     }
--- BackroomSessions_TeamB/View/MenuOptionViewCell.swift
+++ BackroomSessions_TeamB/View/MenuOptionViewCell.swift
@@ -13,11 +13,46 @@ class MenuOptionViewCell: UITableViewCell{
     
     //properties
     
+    let iconImageView: UIImageView = {
+        let iconView = UIImageView()
+        iconView.contentMode = .scaleAspectFit
+        iconView.clipsToBounds = true
+        
+        
+        return iconView
+    }()
     
+    // creating the description label of the hamburguer menu
+    let descriptionLabel: UILabel = {
+        let optionLabel = UILabel()
+        optionLabel.textColor  = .white
+        optionLabel.font = UIFont.systemFont(ofSize: 16)
+        optionLabel.text = "Sample Text"
+        
+        return optionLabel
+    }()
     
     //Init
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .darkGray
+        
+        //adding the view for icon of the hamberguer menu option
+        addSubview(iconImageView)
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        iconImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        
+        //adding the view for descrition label of the hamberguer menu option
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 12).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,4 +60,6 @@ class MenuOptionViewCell: UITableViewCell{
     }
     
     //Funtions
+    
+    
 }
