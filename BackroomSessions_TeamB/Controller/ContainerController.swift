71947ff0e241a9a45b9bc73dc2cd2f1ad137ca97
Isumy Aguila <iagui032@fiu.edu>
2019-12-05 01:55:00 -0500

    So far I have added everything programmatically, that way the project does not depend on the story board, and it would not crash. The project is ready for performing the action to got to the different controller. This actions needs to be called from inside of the func  "didSelectMenuOption", which is in the ContainerController.

--- BackroomSessions_TeamB/Controller/ContainerController.swift
+++ BackroomSessions_TeamB/Controller/ContainerController.swift
@@ -13,7 +13,7 @@ class ContainerController: UIViewController{
     
     //Properties
     //this is created only one time
-    var menuController: UIViewController!
+    var menuController: MenuController!
     //this is the controller shown on top of the ContainerController
     var centerController: UIViewController!
     //keeps track of wheter or not we are showing or hiding the menu.It is false 'coz when the app starts the menu is hiden.
@@ -48,6 +48,7 @@ class ContainerController: UIViewController{
         if menuController == nil{
             //add the menu controller here
             menuController = MenuController()
+            menuController.delegate = self
             view.insertSubview(menuController.view, at: 0)
             addChildViewController(menuController)
             menuController.didMove(toParentViewController: self)
@@ -57,7 +58,7 @@ class ContainerController: UIViewController{
     
     //this function is to show the MenuController.
     //it receive a boolean variable for whether or not we are show or hiding the menu
-    func showMenuCOntroller(shouldExpand: Bool){
+    func animatHamburgerMenu(shouldExpand: Bool, menuOption: MenuOption?){
         if shouldExpand{
             
             //show menu with animation function to the right
@@ -75,9 +76,43 @@ class ContainerController: UIViewController{
                 self.centerController.view.frame.origin.x = 0
                 
                 },completion: nil )
+            
+            UIView.animate(withDuration:0.3 ,delay: 0, usingSpringWithDamping: 0.8,initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                
+                self.centerController.view.frame.origin.x = 0
+                
+            }){(_) in
+                //unwrapping menuOtion in the next line
+                guard let menuOption = menuOption else {return}
+                
+                self.didSelectMenuOption(menuOption: menuOption)
+            }
         }
             
      }
+    
+    //This function handles the options selected in the hamburguer menu
+    //Here is where we go to the controller according to the option that was selected from the menu.
+    func didSelectMenuOption(menuOption: MenuOption){
+        switch menuOption{
+            
+            
+        case .Events:
+            //Go to Event controller
+            print("Show Events Controller")
+        case .Artist:
+            //Go to Artist controller
+            print("Show Artist Controller")
+        case .Favorites:
+            //Go to Favorite controller
+            print("Show Favorite")
+        case .Registration:
+            //Go to Registration controller
+            print("Show Registrations" )
+        }
+        
+    }
+    
 }
 
 extension ContainerController: HomeControllerDelegate{
@@ -90,6 +125,6 @@ extension ContainerController: HomeControllerDelegate{
         
         //every time the hamburguer button is pressed it changes the state of isExpanded to the oposite of what it was.
         isExpanded = !isExpanded
-        showMenuCOntroller(shouldExpand: isExpanded)
+        animatHamburgerMenu(shouldExpand: isExpanded, menuOption: menuOption)
     }
 }
