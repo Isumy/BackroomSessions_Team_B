e0a02b1ca350f62a3d7a047a44d4c214f21f3e53
Isumy Aguila <iagui032@fiu.edu>
2019-12-09 14:25:04 -0500

    Allowed to present the Event Controller when the user clicks on the Event option of the Menu. The Event Controller willAppear in blue for testing purposes.Created SettingController for Testing purposes.

--- BackroomSessions_TeamB/Controller/EventsController.swift
+++ BackroomSessions_TeamB/Controller/EventsController.swift
@@ -14,6 +14,8 @@ class EventsController: UIViewController{
     //Properties
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        view.backgroundColor = .blue
     }
     
     //Init
---
+++ BackroomSessions_TeamB/Controller/SettingsController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsController.swift
+//  BackroomSessions_TeamB
+//
+//  Created by Isumy Aguila on 12/9/19.
+//  Copyright © 2019 Isumy Aguila. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsController: UIViewController{
+    
+    //Properties
+    
+    //Initialize
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
