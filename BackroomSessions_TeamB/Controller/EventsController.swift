8d31be45fb0411a27bef0613205eaf514e3e31f6
Isumy Aguila <iagui032@fiu.edu>
2019-12-12 07:55:20 -0500

    Configuring Data collection with 2 sessions

--- BackroomSessions_TeamB/Controller/EventsController.swift
+++ BackroomSessions_TeamB/Controller/EventsController.swift
@@ -108,13 +108,13 @@ class EventsController: UIViewController, UICollectionViewDelegate, UICollection
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.section == 1{ 
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imagesCellId, for: indexPath) as! ImagesCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: albumsCellId, for: indexPath) as! AlbumsCell
             
             
             
             return cell
         }
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: albumsCellId  , for: indexPath) as! AlbumsCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: imagesCellId  , for: indexPath) as! ImagesCell
         
         return cell
     }
@@ -136,17 +136,91 @@ class EventsController: UIViewController, UICollectionViewDelegate, UICollection
 
 //Classes for the cells
 
-class ImagesCell: UICollectionViewCell {
+class ImagesCell: UICollectionViewCell , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    //Properties
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 30
+        layout.scrollDirection = .horizontal
+        //Initializing collection VIew with frame and layout
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
+        
+        return cv
+    }()
+    
+    let cellIdentifier = "cellIdentifier"
+    //Init
     override init(frame: CGRect){
     super.init(frame: frame)
-         backgroundColor = .red
+        
+        
+        //Calling setup for collection view cell initialization
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
+    
+    //funtions
+    
+    func setup(){
+        backgroundColor = .red
+        addSubview(collectionView)
+        
+        collectionView.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0 , paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        //registering IconCell
+        collectionView.register(IconCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        }
+    //Implementing required methods in order to conform to the delegates
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! IconCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 200, height: frame.height - 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    }
+    
+    
+    //Creating inner class for icon cell
+    private class IconCell: UICollectionViewCell{
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            backgroundColor = .blue
+            
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }// End of Icon class
+    
+}//end of Imagecell Class
+
+
+
+
 class AlbumsCell: UICollectionViewCell {
     
     override init(frame: CGRect){
@@ -157,4 +231,4 @@ class AlbumsCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
+}//End of AlbumCell class
